de4f5109a0451f938fd3e701906f5ff3b5ef0ef5
IAmPatch01 <thomas.gibson01@outlook.com>
2026-01-27 19:27:10 -0700

    Change Shape

--- app/src/main/java/com/example/watchdogs/Shape.java
+++ app/src/main/java/com/example/watchdogs/Shape.java
@@ -4,20 +4,5 @@ public abstract class Shape {
 
     public  int x;
     public int y;
-}
-
-public class Triangle implements Shape {
-
-    public int base;
-    public int height;
-
-    public Triangle (int x, int y, int base, int height) {
-        this.x = x;
-        this.y = y;
-        this.base = base;
-        this.height = height;
-        String color = "turquoise";
-    }
-
-
+    public String color = "red";
 }
---
+++ app/src/main/java/com/example/watchdogs/Triangle.java
@@ -0,0 +1,14 @@
+package com.example.watchdogs;
+public class Triangle implements Shape {
+
+    public int base;
+    public int height;
+
+    public Triangle(int x, int y, int base, int height) {
+        this.x = x;
+        this.y = y;
+        this.base = base;
+        this.height = height;
+        String color = "turquoise";
+    }
+}
